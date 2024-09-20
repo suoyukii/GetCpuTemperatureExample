2ec0641d4cd43557d6a96944042296249b10c57e
suoyukii <heydon-lee@outlook.com>
2024-09-20 19:48:23 +0800

    first commit

---
+++ GetCpuTemperatureExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+while (true)
+{
+    UpdateVisitor updateVisitor = new();
+    Computer computer = new();
+    computer.Open();
+    computer.IsCpuEnabled = true;
+    computer.Accept(updateVisitor);
+    foreach (var hardware in computer.Hardware)
+    {
+        if (hardware.HardwareType is HardwareType.Cpu)
+        {
+            foreach (var sensor in hardware.Sensors)
+            {
+                if (sensor.SensorType == SensorType.Temperature)
+                    Console.Write("{0} : {1:F}\t", sensor.Name, sensor.Value);
+            }
+
+            Thread.Sleep(500);
+            Console.WriteLine();
+        }
+    }
+
+    computer.Close();
+}
+
+internal class UpdateVisitor : IVisitor
+{
+    public void VisitComputer(IComputer computer) => computer.Traverse(this);
+
+    public void VisitHardware(IHardware hardware)
+    {
+        hardware.Update();
+        foreach (var subHardware in hardware.SubHardware) subHardware.Accept(this);
+    }
+
+    public void VisitSensor(ISensor sensor)
+    {
+    }
+
+    public void VisitParameter(IParameter parameter)
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center">GetCpuTemperatureExample</h1>
+<h4 align="center">Get Cpu Temperature Example for .Net 8</h4>
+
+<p align="center">
+  <a href="https://dotnet.microsoft.com">
+    <img src="http://img.shields.io/badge/.Net-8.0-1e90ff.svg"/>
+  </a>
+</p>
